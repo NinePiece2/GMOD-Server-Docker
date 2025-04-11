db417b4ca8c13581ae176ee9cd2e3aff8cd0a8c7
NinePiece2 <romit.sagu@hotmail.com>
2025-04-11 17:14:53 -0400

    Update

--- easygmod.sh
+++ easygmod.sh
@@ -32,99 +32,99 @@ fi
 
 echo "Mounted other game content"
 # Edit server config file
-touch ${SERVERCFG}
-if [ ! -z "${HOSTNAME}" ]
-then
-    if grep -q 'hostname' ${SERVERCFG}
-    then
-        sed -i 's`hostname.*`hostname "'"${HOSTNAME}"'"`' ${SERVERCFG}
-    else
-        echo "hostname \"${HOSTNAME}\"" >> ${SERVERCFG}
-    fi
-fi
-if [ ! -z "${ALLTALK}" ]
-then
-    if grep -q 'sv_alltalk' ${SERVERCFG}
-    then
-        sed -i 's`sv_alltalk.*`sv_alltalk '${ALLTALK}'`' ${SERVERCFG}
-    else
-        echo "sv_alltalk ${ALLTALK}" >> ${SERVERCFG}
-    fi
-fi
-if [ ! -z "${VOICEICON}" ]
-then
-    if grep -q 'mp_show_voice_icons' ${SERVERCFG}
-    then
-        sed -i 's`mp_show_voice_icons.*`mp_show_voice_icons '${VOICEICON}'`' ${SERVERCFG}
-    else
-        echo "mp_show_voice_icons ${VOICEICON}" >> ${SERVERCFG}
-    fi
-fi
-if [ ! -z "${MAXFILESIZE}" ]
-then
-    if grep -q 'net_maxfilesize' ${SERVERCFG}
-    then
-        sed -i 's`net_maxfilesize.*`net_maxfilesize '${MAXFILESIZE}'`' ${SERVERCFG}
-    else
-        echo "net_maxfilesize ${MAXFILESIZE}" >> ${SERVERCFG}
-    fi
-fi
-if [ ! -z "${DOWNLOADURL}" ]
-then
-    if grep -q 'sv_downloadurl' ${SERVERCFG}
-    then
-        sed -i 's`sv_downloadurl.*`sv_downloadurl "'"${DOWNLOADURL}"'"`' ${SERVERCFG}
-    else
-        echo "sv_downloadurl \"${DOWNLOADURL}\"" >> ${SERVERCFG}
-    fi
-    if grep -q 'sv_allowdownload' ${SERVERCFG}
-    then
-        sed -i 's`sv_allowdownload.*`sv_allowdownload 1`' ${SERVERCFG}
-    else
-        echo "sv_allowdownload 1" >> ${SERVERCFG}
-    fi
-    if grep -q 'sv_allowupload' ${SERVERCFG}
-    then
-        sed -i 's`sv_allowupload.*`sv_allowupload 1`' ${SERVERCFG}
-    else
-        echo "sv_allowupload 1" >> ${SERVERCFG}
-    fi
-fi
-if [ ! -z "${LOADINGURL}" ]
-then
-    if grep -q 'sv_loadingurl' ${SERVERCFG}
-    then
-        sed -i 's`sv_loadingurl.*`sv_loadingurl "'"${LOADINGURL}"'"`' ${SERVERCFG}
-    else
-        echo "sv_loadingurl \"${LOADINGURL}\"" >> ${SERVERCFG}
-    fi
-fi
-if [ ! -z "${PASSWORD}" ]
-then
-    if grep -q 'sv_password' ${SERVERCFG}
-    then
-        sed -i 's`sv_password.*`sv_password "'"${PASSWORD}"'"`' ${SERVERCFG}
-    else
-        echo "sv_password \"${PASSWORD}\"" >> ${SERVERCFG}
-    fi
-fi
-if [ ! -z "${RCONPASSWORD}" ]
-then
-    if grep -q 'rcon_password' ${SERVERCFG}
-    then
-        sed -i 's`rcon_password.*`rcon_password "'"${RCONPASSWORD}"'"`' ${SERVERCFG}
-    else
-        echo "rcon_password \"${RCONPASSWORD}\"" >> ${SERVERCFG}
-    fi
-fi
-if ! grep -q 'exec banned_ip.cfg' ${SERVERCFG}
-then
-    echo "exec banned_ip.cfg" >> ${SERVERCFG}
-fi
-if ! grep -q 'exec banned_user.cfg' ${SERVERCFG}
-then
-    echo "exec banned_user.cfg" >> ${SERVERCFG}
-fi
+# touch ${SERVERCFG}
+# if [ ! -z "${HOSTNAME}" ]
+# then
+#     if grep -q 'hostname' ${SERVERCFG}
+#     then
+#         sed -i 's`hostname.*`hostname "'"${HOSTNAME}"'"`' ${SERVERCFG}
+#     else
+#         echo "hostname \"${HOSTNAME}\"" >> ${SERVERCFG}
+#     fi
+# fi
+# if [ ! -z "${ALLTALK}" ]
+# then
+#     if grep -q 'sv_alltalk' ${SERVERCFG}
+#     then
+#         sed -i 's`sv_alltalk.*`sv_alltalk '${ALLTALK}'`' ${SERVERCFG}
+#     else
+#         echo "sv_alltalk ${ALLTALK}" >> ${SERVERCFG}
+#     fi
+# fi
+# if [ ! -z "${VOICEICON}" ]
+# then
+#     if grep -q 'mp_show_voice_icons' ${SERVERCFG}
+#     then
+#         sed -i 's`mp_show_voice_icons.*`mp_show_voice_icons '${VOICEICON}'`' ${SERVERCFG}
+#     else
+#         echo "mp_show_voice_icons ${VOICEICON}" >> ${SERVERCFG}
+#     fi
+# fi
+# if [ ! -z "${MAXFILESIZE}" ]
+# then
+#     if grep -q 'net_maxfilesize' ${SERVERCFG}
+#     then
+#         sed -i 's`net_maxfilesize.*`net_maxfilesize '${MAXFILESIZE}'`' ${SERVERCFG}
+#     else
+#         echo "net_maxfilesize ${MAXFILESIZE}" >> ${SERVERCFG}
+#     fi
+# fi
+# if [ ! -z "${DOWNLOADURL}" ]
+# then
+#     if grep -q 'sv_downloadurl' ${SERVERCFG}
+#     then
+#         sed -i 's`sv_downloadurl.*`sv_downloadurl "'"${DOWNLOADURL}"'"`' ${SERVERCFG}
+#     else
+#         echo "sv_downloadurl \"${DOWNLOADURL}\"" >> ${SERVERCFG}
+#     fi
+#     if grep -q 'sv_allowdownload' ${SERVERCFG}
+#     then
+#         sed -i 's`sv_allowdownload.*`sv_allowdownload 1`' ${SERVERCFG}
+#     else
+#         echo "sv_allowdownload 1" >> ${SERVERCFG}
+#     fi
+#     if grep -q 'sv_allowupload' ${SERVERCFG}
+#     then
+#         sed -i 's`sv_allowupload.*`sv_allowupload 1`' ${SERVERCFG}
+#     else
+#         echo "sv_allowupload 1" >> ${SERVERCFG}
+#     fi
+# fi
+# if [ ! -z "${LOADINGURL}" ]
+# then
+#     if grep -q 'sv_loadingurl' ${SERVERCFG}
+#     then
+#         sed -i 's`sv_loadingurl.*`sv_loadingurl "'"${LOADINGURL}"'"`' ${SERVERCFG}
+#     else
+#         echo "sv_loadingurl \"${LOADINGURL}\"" >> ${SERVERCFG}
+#     fi
+# fi
+# if [ ! -z "${PASSWORD}" ]
+# then
+#     if grep -q 'sv_password' ${SERVERCFG}
+#     then
+#         sed -i 's`sv_password.*`sv_password "'"${PASSWORD}"'"`' ${SERVERCFG}
+#     else
+#         echo "sv_password \"${PASSWORD}\"" >> ${SERVERCFG}
+#     fi
+# fi
+# if [ ! -z "${RCONPASSWORD}" ]
+# then
+#     if grep -q 'rcon_password' ${SERVERCFG}
+#     then
+#         sed -i 's`rcon_password.*`rcon_password "'"${RCONPASSWORD}"'"`' ${SERVERCFG}
+#     else
+#         echo "rcon_password \"${RCONPASSWORD}\"" >> ${SERVERCFG}
+#     fi
+# fi
+# if ! grep -q 'exec banned_ip.cfg' ${SERVERCFG}
+# then
+#     echo "exec banned_ip.cfg" >> ${SERVERCFG}
+# fi
+# if ! grep -q 'exec banned_user.cfg' ${SERVERCFG}
+# then
+#     echo "exec banned_user.cfg" >> ${SERVERCFG}
+# fi
 
 echo "Server config file updated"
 echo "Server config file location: ${SERVERCFG}"
